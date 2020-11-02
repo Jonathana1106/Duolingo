73207dfa73777f51e44de8f725b5c150c2af4700
AmandaCastro <43190871+AmandaCastro@users.noreply.github.com>
2020-11-02 13:34:04 -0600

    Documentos

---
+++ DuolingoWeb/src/models/usuario.js
@@ -0,0 +1,20 @@
+const { STRING_TYPE } = require("@angular/compiler");
+const { interval } = require("rxjs");
+
+const mongoose = required('mongoose');
+const { Schema } = mongoose;
+
+const UsuarioSchema = new Schema({
+    nombre: {type: String, requiered:true},
+    edad: {type: Number, required:true},
+    sexo: {type:String, require:true},
+    correo: {type:String, required:true},
+    contraseña: {type:String, required:true},
+    paisOrigen: {type:String, required:true},
+    idiomaAprender: {type:String, required:true},
+    idiomaEnseñar: {type:String, requiered:true},
+    hobbies: {type:String, requiered:true},
+    medio: {type:String, requiered:true}
+});
+
+module.exports = mongoose.model('usuario', NoteSchema)
