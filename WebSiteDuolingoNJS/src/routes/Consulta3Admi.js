0d09b6bc83ef3df82d34edf3f1a14b05294336be
AmandaCastro <43190871+AmandaCastro@users.noreply.github.com>
2020-11-22 22:55:25 -0600

    r

---
+++ WebSiteDuolingoNJS/src/routes/Consulta3Admi.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const express = require('express');
+const router = express.Router();
+
+const registroCant = require('../models/User');
+
+router.get('/ConsultaAdmi/Consulta3Admi', (req, res) => {
+    res.render('ConsultaEnseniar/enseniar');
+});
+
+
+router.post("/ConsultaEnseniar/enseniar", async (req, res) => {
+    const {idiomasteach} = req.body;
+    const CantEnseniar = await registroCant.aggregate([
+            {
+                {
+                    "$unwind": "$idiomasteach"
+                }, {
+                    "$group": {
+                        "_id": "$idiomasteach.idioma",
+                        "total_idioma": {
+                            "$sum": 1
+                        }
+                    }
+                }
+            ]
+        );
+    }
+]);
+/*for(var x=0; x<=Todos.length;x++){
+        var obj = Todos[x];
+        console.log(obj);
+        res.render('ConsultaTodoRegistro/Todosregistros',{obj});
+    }*/
+res.render('ConsultaEnseniar/enseniar', {CantEnseniar});});module.exports = router;
+
+
+
